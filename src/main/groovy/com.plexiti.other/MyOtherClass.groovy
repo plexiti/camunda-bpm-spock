4f84722da4bb5a8842817af5ee59ac9d5b5e26e4
Martin Schimak <martin.schimak@plexiti.com>
2014-10-08 15:26:51 +0200

    chore(script): remove class in order to allow deployment on vanilla tomcat

--- src/main/groovy/com.plexiti.other/MyOtherClass.groovy
+++
@@ -1,7 +0,0 @@
-package com.plexiti.other
-
-/**
- * @author Martin Schimak <martin.schimak@plexiti.com>
- */
-class MyOtherClass {
-}
--- src/main/resources/com/plexiti/test/MyTestScript.groovy
+++ src/main/resources/com/plexiti/test/MyTestScript.groovy
@@ -1,11 +1,9 @@
 package com.plexiti.test
 
-import com.plexiti.other.MyOtherClass
 import org.camunda.bpm.engine.ProcessEngine
 import org.camunda.bpm.engine.ProcessEngineConfiguration
 
 println("Here we are - script with $execution.id!")
-def myOtherClass = new MyOtherClass()
 def myJarClass = new ProcessEngineConfiguration() {
     @Override
     ProcessEngine buildProcessEngine() {
