9eb602ef929087e4d4357116e0d783d86d44afdc
Martin Schimak <martin.schimak@plexiti.com>
2014-11-10 22:20:34 +0100

    feature(extension): spock annotation for deployment

---
+++ src/main/java/org/camunda/bpm/extension/spock/Deployment.java
@@ -0,0 +1,21 @@
+package org.camunda.bpm.extension.spock;
+
+import org.spockframework.runtime.extension.ExtensionAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Martin Schimak <martin.schimak@plexiti.com>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@ExtensionAnnotation(DeploymentExtension.class)
+public @interface Deployment {
+  
+  /** Specify resources that make up the process definition. */
+  String[] resources() default {};
+
+}
